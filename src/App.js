9da17d1d01c024e9026581ca40da32c8fdffba63
Pasindu Shavinda <pasindu.shv@gmail.com>
2022-05-29 12:12:49 +0530

    changes done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { HashRouter, Route } from "react-router-dom";
 import Shv_All_Taxi_Reservations from "./components/shv_TaxiReservation/shv_All_Taxi_Reservations";
 import Shv_Create_Taxi_Reservations from "./components/shv_TaxiReservation/shv_Create_Taxi_Reservations";
 import Shv_Taxi_Reservations_Home_Page from "./components/shv_TaxiReservation/shv_Taxi_Reservations_Home_Page";
